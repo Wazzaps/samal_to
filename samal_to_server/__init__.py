793b496b84ae193429b0754498edcfb10b647766
David Shlemayev <david.shlemayev@gmail.com>
2021-04-20 21:21:16 +0300

    Initial commit

---
+++ samal_to_server/__init__.py
@@ -0,0 +1,17 @@
+from flask import Flask
+import os
+
+app = Flask(__name__)
+app.config.from_mapping(
+    SECRET_KEY='dev',
+    DATABASE=os.path.join(app.instance_path, 'samal_to.sqlite'),
+)
+
+try:
+    os.makedirs(app.instance_path)
+except OSError:
+    pass
+
+@app.route('/')
+def hello():
+    return 'Hello, World!'
